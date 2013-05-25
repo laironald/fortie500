9fbbac32745bd91a336f01a662d1ae84aee2fc71
Ron MacBook Air <laironald@gmail.com>
2013-05-24 23:11:05 -0500

    modified 2013 to include URL

--- fortie500_2013.py
+++ fortie500_2013.py
@@ -3,20 +3,26 @@ import codecs
 import BeautifulSoup
 import urllib2
 
-url = {2013:"http://money.cnn.com/magazines/fortune/fortune500/2013/full_list/index.html?iid=F500_sp_full"}
+url = "http://money.cnn.com/magazines/fortune/fortune500/2013/full_list/index.html?iid=F500_sp_full"
+baseurl = "http://money.cnn.com"
 
-html = urllib2.urlopen(url[2013])
+html = urllib2.urlopen(url)
 html = "".join([x for x in html])
 
 soup = BeautifulSoup.BeautifulSoup(html)
 soup = soup.find(id="f500-list")
+soup = soup.find(id="content-placeholder")
 
 cnt = 0
 out = codecs.open("output/f500-2013.txt", encoding="utf-8", mode="wb")
-data = [["number", "company"]]
-for span in soup.findAll("span"):
-    if "name" in span['class'].split():
-        cnt += 1
-        out.write(u"{0}\t{1}\n".format(cnt, span.text))
+out.write("rank\turl\tcompany\n")
+for li in soup.findAll("li"):
+    cnt += 1
+    out.write(u"{0}\t".format(cnt))
+    out.write(u"{0}\t".format(baseurl+li.findChild("a")["href"]))
+    for span in li.findAll("span"):
+        if "name" in span['class'].split():
+            out.write(u"{0}\n".format(span.text))
+            continue
 
 out = None
